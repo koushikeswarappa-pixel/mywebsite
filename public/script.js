ce47942040fc6748d5142eef246a489a0d95c95e
Koushik <koushikeswarappa@gmail.com>
2026-04-13 02:23:36 -0700

    final logo website

--- public/script.js
+++ public/script.js
@@ -1,11 +1,25 @@
+function scrollToSection() {
+  document.getElementById("projects").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+document.querySelectorAll(".dropdown > a").forEach(item => {
+  item.addEventListener("click", function(e) {
+    e.preventDefault();
+    let menu = this.nextElementSibling;
+
+    menu.style.display =
+      menu.style.display === "block" ? "none" : "block";
+  });
+});
+
 document.getElementById("form").onsubmit = async (e) => {
   e.preventDefault();
 
   await fetch("/contact", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       name: name.value,
       email: email.value,
