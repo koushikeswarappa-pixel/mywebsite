9cff96d60827eda4d3ad829adc72b57e1d272ce1
Koushik <koushikeswarappa@gmail.com>
2026-04-12 23:12:01 -0700

    fix port binding

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.post("/contact", (req, res) => {
 // IMPORTANT: works for both local + Render
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
